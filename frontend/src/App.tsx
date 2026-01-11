7d2288c1bc3b18a37601214eafdbc53b02eb820f
Sarvesh <codewithsarvesh@gmail.com>
2026-01-11 12:23:08 +0530

    toggling with dummy data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,51 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import type { Schedule, Topic } from './shared/types'
+import { mockSchedule } from './shared/mockData'
 
 function App() {
-  const [count, setCount] = useState(0)
+ const [schedule, setSchedule] = useState<Schedule>(mockSchedule);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+ const toggleStatus = (dayDate:string, topicID:string) => {
+    setSchedule({
+      ...schedule,
+      days: schedule.days.map((day)=>
+        day.date===dayDate ? {
+          ...day,
+          topics: day.topics.map(topic => 
+            topic.id === topicID ? {...topic,completed: !topic.completed} : topic
+          )
+        } : day
+      )
+    })
+ }
+
+ return (
+  <>
+   <h1>{schedule.id}</h1>
+   {schedule.days.map((day)=>{
+    return (
+    <div key={day.date}>
+      <p>{day.date}</p>
+      <ul>
+        {day.topics.map((topic)=>{
+          return (
+            <div key={topic.id}>
+            <li>{topic.title}</li>
+            <p>{topic.completed ? "Completed" : "Not Completed"}</p>
+            <button onClick={() => toggleStatus(day.date,topic.id)}>Click</button>
+            </div>
+          );
+          
+        })}
+      </ul>
+    </div>);
+    
+   })}
+   </>
+ 
+ );
+
 }
 
 export default App
